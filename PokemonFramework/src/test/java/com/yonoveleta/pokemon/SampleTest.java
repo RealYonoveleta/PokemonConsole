ed098bf7f04cbf40bf0cc8dee402171ddcb1cacb
RealYonoveleta <martinezcuestajonas@gmail.com>
2025-03-28 11:50:15 +0100

    added junit as dependency

---
+++ PokemonFramework/src/test/java/com/yonoveleta/pokemon/SampleTest.java
@@ -0,0 +1,14 @@
+package com.yonoveleta.pokemon;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+	@Test
+	public void sampleTest() {
+		assertTrue(true);
+	}
+
+}
